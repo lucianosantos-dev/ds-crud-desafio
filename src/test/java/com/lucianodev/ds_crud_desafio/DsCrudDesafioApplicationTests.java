ca97248802ab61364f4d809235a1ef58a36cdb74
Luciano Santos <lucianodev.contato@gmail.com>
2025-11-23 01:37:21 -0300

    Projeto criado

---
+++ src/test/java/com/lucianodev/ds_crud_desafio/DsCrudDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianodev.ds_crud_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCrudDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
