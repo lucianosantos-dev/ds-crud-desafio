f049382596f30ed09bc210085c208e0574eec04a
Luciano Santos <lucianodev.contato@gmail.com>
2025-11-23 10:20:41 -0300

    Implementação do CRUD na camada de serviços

---
+++ src/main/java/com/lucianodev/ds_crud_desafio/services/ClientService.java
@@ -0,0 +1,59 @@
+package com.lucianodev.ds_crud_desafio.services;
+
+import com.lucianodev.ds_crud_desafio.dto.ClientDTO;
+import com.lucianodev.ds_crud_desafio.entities.Client;
+import com.lucianodev.ds_crud_desafio.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).orElseThrow();
+        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> clients = repository.findAll(pageable);
+        return clients.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto, Client entity) {
+        Client client = new Client();
+        copyDTOToEntity(dto, entity);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        Client entity = repository.getReferenceById(id);
+        copyDTOToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public void copyDTOToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
+}
